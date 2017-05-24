00fd103590b431a5c79f3069475862fac6d117fb
Cameron Jensen and Mark Fisher <mark@>
2017-05-24 15:29:19 -0700

    add word submit and check

--- app/src/main/java/com/epicodus/bogglesolitaire/GameplayActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/GameplayActivity.java
@@ -1,10 +1,14 @@
 package com.epicodus.bogglesolitaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
@@ -14,13 +18,16 @@ import butterknife.ButterKnife;
 
 import static java.util.concurrent.ThreadLocalRandom.current;
 
-public class GameplayActivity extends AppCompatActivity {
+public class GameplayActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind (R.id.wordSubmitText) Button mWordSubmitText;
+    @Bind (R.id.answerText) EditText mAnswerText;
     GridView gridView;
     private Character[] allLetters = new Character[] {
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
     private Character[] vowels = new Character[] {'A', 'E', 'I', 'O', 'U', 'Y'};
     private ArrayList<Character> currentLetterSet= new ArrayList<>();
+    private ArrayList<String> results = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,20 @@ public class GameplayActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         gridView = (GridView) findViewById(R.id.baseGridView);
         gridView.setAdapter(new BoggleLetterAdapter(this, currentLetterSet));
+        mWordSubmitText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String word = mAnswerText.getText().toString();
+        if (isValidWordInLetterSet(word, currentLetterSet) && !results.contains(word.toUpperCase())) {
+           results.add(word.toUpperCase());
+            for(int i=0; i<results.size(); i++){
+                Log.d("result", results.get(i));
+            }
+        } else {
+            Toast.makeText(GameplayActivity.this, "Try Again", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public ArrayList<Character> generateLetters(){
@@ -45,7 +66,8 @@ public class GameplayActivity extends AppCompatActivity {
         return output;
     }
 
-    public boolean isValidWordInLetterSet(String word, ArrayList<Character> letterSet){
+    public boolean isValidWordInLetterSet(String word, ArrayList<Character> originalLetterSet){
+        ArrayList<Character> letterSet = (ArrayList) originalLetterSet.clone();
         boolean returnVal = false;
         if(word.length()<3 || word.length()>8){
             returnVal =  false;
